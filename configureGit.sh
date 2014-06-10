477732627665f2956cb64e792d7e9b8728cff4a0
Devin Wieker <devinwieker@Devins-MacBook-Air.local>
2014-06-09 19:22:42 -0700

    Created config files

---
+++ configureGit.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# This script configures git whenever you start working
+# on multiple machines. Run it any time you decide to work
+#  with git on a new computer
+git config --global user.name "Devin Wieker"
+git config --global user.email "dwieker@calpoly.edu"
+git config --global color.ui "auto"
+git config --global core.editor "nano"
